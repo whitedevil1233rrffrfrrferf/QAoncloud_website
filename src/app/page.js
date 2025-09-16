cd6d0f89b26033119c2d09fb426f1b94bdb91d5f
whitedevil1233rrffrfrrferf <121005612+whitedevil1233rrffrfrrferf@users.noreply.github.com>
2025-09-16 10:54:59 +0530

    css_file_changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Banner from "@/components/Software_testing/Banner";
-import Map from "@/components/HomePage/Map";
-import Services from "@/components/HomePage/Services";
-import Solutions from "@/components/HomePage/Solutions";
-import Industries from "@/components/HomePage/Industries";
-import WhyQAonCloud from "@/components/HomePage/WhyQAonCloud";
-import QualitySoftware from "@/components/HomePage/QualitySoftware";
-import ClientTestimonials from "@/components/HomePage/ClientTestimonials";
+import Banner from "../components/Software_testing/Banner";
+import Map from "../components/HomePage/Map";
+import Services from "../components/HomePage/Services";
+import Solutions from "../components/HomePage/Solutions";
+import Industries from "../components/HomePage/Industries";
+import WhyQAonCloud from "../components/HomePage/WhyQAonCloud";
+import QualitySoftware from "../components/HomePage/QualitySoftware";
+import ClientTestimonials from "../components/HomePage/ClientTestimonials";
 export default function Home() {
   return (
     <>
--- src/components/Software_testing/Banner.jsx
+++ src/components/Software_testing/Banner.jsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import styles from "./banner.module.css";
+import styles from "./Banner.module.css";
 import Image from "next/image";
 
 import Button from "../Common/Button/Button";
